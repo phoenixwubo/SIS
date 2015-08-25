23d407c88ed76ea38270935ad56a7ef5fceaf965
phoenixwubo <legend_of_phenix@163.com>
2015-08-25 23:51:44 +0800

    修复将导入学生信息改为窗口模式产生的问题

--- app/webroot/js/app/controller/Students.js
+++ app/webroot/js/app/controller/Students.js
@@ -76,7 +76,7 @@ Ext.define('SIS.controller.Students', {
     	//console.log('Double clicked on '+record.get('name'));
     	
 
-    	console.log("编辑");
+//    	console.log("编辑");
     	var grid=this.getStudentList();
     	//var grid=Ext.get('studentlist');
     	var sm=grid.getSelectionModel();
@@ -101,11 +101,11 @@ Ext.define('SIS.controller.Students', {
     studentProfile:function(gird,record){
     	var view=Ext.widget('studentprofile');
     	view.down('form').loadRecord(record);
-    	console.log(record);
+//    	console.log(record);
     },
 
     updateStudent:function(button){
-    	console.log('clicked the save button');
+//    	console.log('clicked the save button');
         var win    = button.up('window'),
         form   = win.down('form'),
         record = form.getRecord(),
@@ -137,7 +137,7 @@ Ext.define('SIS.controller.Students', {
     	var addStudentview=Ext.widget('addstudent');
     },
     deleteStudents:function(){
-    	console.log("删除");
+//    	console.log("删除");
     	var grid=this.getStudentList();
     	//var grid=Ext.get('studentlist');
     	var sm=grid.getSelectionModel();
@@ -145,7 +145,7 @@ Ext.define('SIS.controller.Students', {
     	if(sm.getSelection().length==0){
     		alert('请选择一条记录')}
     	else{
-    		console.log(sm.getSelection()[0].data.id);
+//    		console.log(sm.getSelection()[0].data.id);
     		
     		Ext.MessageBox.confirm('删除记录','确定要删除'+sm.getSelection()[0].data.stu_name+'？',function(btn){
     			if(btn == 'yes'){
@@ -194,10 +194,10 @@ Ext.define('SIS.controller.Students', {
     	//var addStudentview=Ext.widget('studentadd');
     },
     searchStudent:function(button){
-    	console.log(Ext.get('KeyWord').getValue());
-    	console.log( button.up('studentlist').down('KeyWord'));
+//    	console.log(Ext.get('KeyWord').getValue());
+//    	console.log( button.up('studentlist').down('KeyWord'));
     	key= button.up('studentlist').down('KeyWord').getValue();
-    	console.log(key);
+//    	console.log(key);
     }
     ,
     onPanelRendered: function(panel) {
@@ -214,7 +214,7 @@ Ext.define('SIS.controller.Students', {
     ,
     selectDepartment:function(comboBox){
     	department_id=comboBox.getValue();
-    	console.log(department_id);
+//    	console.log(department_id);
     	var dept_number;
 		
 	
@@ -249,7 +249,7 @@ Ext.define('SIS.controller.Students', {
 								}
 								if (result.success) { // #3
 									dept_number=result.data.dept_number;
-									console.log(dept_number);
+//									console.log(dept_number);
 									store.on('beforeload', function (store, options) {
 
 							            var new_params = { dept_number:dept_number };
@@ -282,7 +282,7 @@ Ext.define('SIS.controller.Students', {
     searchStudent:function(button,grid){
     	var grid=button.up('panel');
     	var stu_name=grid.down('#name').getValue();
-    	console.log(stu_name);
+//    	console.log(stu_name);
     	var store=this.getStudentsStore();
 	
 		store.on('beforeload', function (store, options) {
@@ -301,7 +301,7 @@ Ext.define('SIS.controller.Students', {
 //    	console.log('下载！');
     	var panel=itme.up('panel');
     	var chart=panel.down('chart')
-    	console.log(chart);
+//    	console.log(chart);
     	 Ext.MessageBox.confirm('Confirm Download', 'Would you like to download the chart as an image?', function(choice){
              if(choice == 'yes'){
                  chart.save({
@@ -312,6 +312,6 @@ Ext.define('SIS.controller.Students', {
     },
     onImportStudents:function(button){
     	var importStudent=Ext.widget('importstudent');
-    	console.log(importStudent);
+//    	console.log(importStudent);
     }
   });
--- app/webroot/js/app/view/student/Import.js
+++ app/webroot/js/app/view/student/Import.js
@@ -7,33 +7,39 @@ Ext.define('SIS.view.student.Import' ,{
     title: '导入学生信息',
     fileUpload:true,
     frame:true,
-    items:[{
-    	xtype:'fileuploadfield',
-    	fieldLabel:'上传',
-        name:'import'
-    }],
-    buttons: [{
-        text: '上传数据',
-        handler: function(){
-            var form = this.up('form').getForm();
-            if (form.isValid()) {
-                form.submit({
-                    url: 'students/import/',
-                    waitMsg: 'Uploading...',
-                    success: function (f, a) {
-                        var result = a.result, data = result.data,
-                          name = data.name, size = data.size,
-                        message = Ext.String.format('<b>Message:</b> {0}<br>' +
-                          '<b>FileName:</b> {1}<br>' +
-                          '<b>FileSize:</b> {2}',
-                          result.msg, name, size);
-                        Ext.Msg.alert('Success', message);
-                      },
-                      failure: function (f, a) {
-                        Ext.Msg.alert('Failure', a.result.msg);
-                      }
-                    	}); }
-        }
-    }] 
+    items:{
+    	xtype:'form',
+//    	layout: "form",
+    	id:'importelective',
+	    items:[{
+	    	xtype:'fileuploadfield',
+	    	fieldLabel:'上传',
+	        name:'import'
+	    }],
+	    buttons: [{
+	        text: '上传数据',
+	        handler: function(){
+	            var form = this.up('form').getForm();
+	            if (form.isValid()) {
+	                form.submit({
+	                    url: 'students/import/',
+	                    waitMsg: 'Uploading...',
+	                    success: function (f, a) {
+	                        var result = a.result, data = result.data,
+	                          name = data.name, size = data.size,
+	                        message = Ext.String.format('<b>Message:</b> {0}<br>' +
+	                          '<b>FileName:</b> {1}<br>' +
+	                          '<b>FileSize:</b> {2}',
+	                          result.msg, name, size);
+	                        Ext.Msg.alert('Success', message);
+	                      },
+	                      failure: function (f, a) {
+	                        Ext.Msg.alert('Failure', a.result.msg);
+	                      }
+	                    	}); }
+	        }
+	    }] 
+    }
+
  
 });
